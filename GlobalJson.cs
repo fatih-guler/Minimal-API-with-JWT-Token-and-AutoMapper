1dd860e83219ae2431ebf4e3e765e1ad716dab00
fatih-guler <fatih.guler.1995@gmail.com>
2022-01-01 11:50:12 +0300

    Update GlobalJson.cs

--- GlobalJson.cs
+++ GlobalJson.cs
@@ -1,4 +1,6 @@
-﻿global using AutoMapper;
+﻿// global keywork is making the class reachable from all classes.
+// When classses are defined in this way, no need to add to other classes as 'using ...'
+global using AutoMapper;
 global using Microsoft.AspNetCore.DataProtection;
 global using Microsoft.OpenApi.Models;
 global using MinimalAPI.Data;
